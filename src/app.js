a6abcb957381e6c14113e689a588845f5d88bb3d
MacRat <m@crat.jp>
2017-12-21 21:16:38 +0900

    Enhanced image support

--- src/app.js
+++ src/app.js
@@ -58,11 +58,24 @@ const imageThumbnail = widgets.createType({
 			};
 		}),
 	],
+	debounceWait: 10,
+	findEditRange: function(range) {
+		const ret = {
+			from: { line: range.from.line - 1, ch: 0 },
+			to: { line: range.to.line + 1, ch: 0 },
+		};
+		return ret;
+	},
 	createElement: function(widget) {
 		const img = document.createElement('img');
 		img.src = widget.props.src;
 		img.alt = widget.props.alt;
 		img.title = widget.props.alt;
+		img.classList.add('cm-thumbnail');
+		img.classList.add('cm-thumbnail-image');
+		img.addEventListener('error', function() {
+			this.classList.add('cm-thumbnail-missing');
+		});
 		return img;
 	},
 });
